38591da793ef0d18d37d77f55e0b9909891d6680
Mark Dittmer <markdittmer@google.com>
2015-05-14 14:09:15 -0400

    INCOMPLETE WORK ON BWT: See TODOs and commented-out code for next steps

--- js/foam/dao/index/BWT.js
+++ js/foam/dao/index/BWT.js
@@ -26,7 +26,8 @@ CLASS({
 
   requires: [
     'foam.dao.index.BWTCharGenerator',
-    'foam.dao.index.RotatedStr'
+    'foam.dao.index.RotatedStr',
+    'foam.dao.index.WaveletTree'
   ],
 
   properties: [
@@ -34,6 +35,10 @@ CLASS({
       model_: 'StringProperty',
       name: 'eos',
       defaultValue: String.fromCharCode(0x0000FFFF | 0)
+    },
+    {
+      type: 'foam.dao.index.WaveletTree',
+      name: 'waveletTree_'
     },
     // Phase 1: Input data. Data may be discarded in Phase 2.
     {
@@ -83,31 +88,10 @@ CLASS({
       setter: function(bwt) {
         if ( this.discardData ) this.data = '';
         this.bwt_ = bwt;
+        if ( bwt ) this.waveletTree_ = this.WaveletTree.create({ data: bwt });
       }
-    },
-    {
-      model_: 'BooleanProperty',
-      name: 'discardBWT',
-      defaultValue: true
     },
     // Phase 3. Data may be discarded in Phase 4.
-    {
-      name: 'numBWT_',
-      getter: function() {
-        if ( this.numBWT__.length > 0 ) return this.numBWT__;
-        if ( ! this.bwt ) return [];
-        this.numBWT_ = this.numifyBWT_();
-        return this.numBWT__;
-      },
-      setter: function(numBWT) {
-        if ( this.discardBWT ) this.bwt = '';
-        this.numBWT__ = numBWT;
-      }
-    },
-    {
-      model_: 'ArrayProperty',
-      name: 'numBWT__'
-    },
     {
       name: 'eosIdx_',
       getter: function() {
@@ -127,30 +111,29 @@ CLASS({
       defaultValue: -1
     },
     {
-      name: 'sortedNumBWT_',
+      name: 'sortedBWT_',
       getter: function() {
-        if ( this.sortedNumBWT__.length > 0 ) return this.sortedNumBWT__;
-        if ( this.numBWT_.length === 0 ) return [];
-        this.sortedNumBWT_ = this.sortNumBWT_();
-        return this.sortedNumBWT__;
+        if ( this.sortedBWT__ ) return this.sortedBWT__;
+        if ( ! this.bwt ) return '';
+        this.sortedBWT_ = this.bwt.split('').sort().join('');
+        return this.sortedBWT__;
       },
-      setter: function(sortedNumBWT) {
-        if ( this.discardBWT ) this.bwt = '';
-        this.sortedNumBWT__ = sortedNumBWT;
+      setter: function(sortedBWT) {
+        this.sortedBWT__ = sortedBWT;
       }
     },
     {
-      model_: 'ArrayProperty',
-      name: 'sortedNumBWT__'
+      model_: 'StringProperty',
+      name: 'sortedBWT__'
     },
     {
       model_: 'BooleanProperty',
-      name: 'discardNumBWT',
+      name: 'discardBWT',
       defaultValue: false
     },
     {
       model_: 'BooleanProperty',
-      name: 'discardSortedNumBWT',
+      name: 'discardSortedBWT',
       defaultValue: true
     },
     // Phase 4.
@@ -162,8 +145,8 @@ CLASS({
         return this.bwtMap_;
       },
       setter: function(bwtMap) {
-        if ( this.discardNumBWT ) this.numBWT_ = [];
-        if ( this.discardSortedNumBWT ) this.sortedNumBWT_ = [];
+        if ( this.discardBWT ) this.bwt_ = '';
+        if ( this.discardSortedBWT ) this.sortedBWT_ = '';
         this.bwtMap_ = bwtMap;
       }
     },
@@ -193,23 +176,6 @@ CLASS({
       }
       return rtn;
     },
-    function numifyBWT_() {
-      var bwtCharGenerator = this.BWTCharGenerator.create();
-      var len = this.bwt_.length;
-      var bwt = new Array(len);
-      for ( var i = 0; i < len; ++i ) {
-        if ( this.bwt_[i] === this.eos ) this.eosIdx_ = i;
-        bwt[i] = bwtCharGenerator.generateChar(this.bwt_[i]);
-      }
-      return bwt;
-    },
-    function sortNumBWT_() {
-      var bwt = this.numBWT__;
-      var len = bwt.length;
-      var sortedBWT = bwt.slice(0);
-      sortedBWT.sort();
-      return sortedBWT;
-    },
     function locateEOSIdx_() {
       var bwt = this.bwt;
       var len = bwt.length;
@@ -219,51 +185,79 @@ CLASS({
 
       throw new Error('Expected bwt to contain end-of-string character');
     },
-    function generateBWTMap_() {
-      var fst = this.numBWT_;
-      var snd = this.sortedNumBWT_;
 
-      if ( fst.length !== snd.length )
-        throw new Error('BWT and sorted BWT are not the same size');
-      if ( fst.length === 0 ) return {};
+    // TODO(markdittmer): Trying to do away with two-char representations and
+    // bwt map. This requires an implementation of select(idx) over wavelet
+    // trees and rrrs.
 
-      // Number associated with only end-of-string is:
-      // (16-bit char code, 16-bit counter) = ([eos char code], 0).
-      var numEOS = this.eos.charCodeAt(0) << 16;
-      var idx = this.bsFind_(snd, numEOS, 0, snd.length - 1);
-      var map = {};
-      while ( fst[idx] !== numEOS ) {
-        map[this.numToTwoChar_(fst[idx])] = this.numToTwoChar_(snd[idx]);
-        idx = this.bsFind_(snd, fst[idx], 0, fst.length - 1);
-      }
-      return map;
-    },
-    function bsFind_(arr, value, start, end) {
-      var mid;
-      while ( start <= end ) {
-        mid = Math.floor(start + ((end - start) / 2));
-        if ( arr[mid] === value ) return mid;
-        if ( arr[mid] < value ) start = mid + 1;
-        if ( arr[mid] > value ) end = mid - 1;
-      }
-      return -1;
-    },
+    // function generateBWTMap_() {
+    //   var fst = this.bwt;
+    //   var snd = this.sortedBWT_;
+
+    //   if ( fst.length !== snd.length )
+    //     throw new Error('BWT and sorted BWT are not the same size');
+    //   if ( fst.length === 0 ) return {};
+
+    //   var map = {};
+    //   for ( var i = 0; i < fst.length; ++i ) {
+    //     map[fst[i]] = map[snd[i]];
+    //     // var fstChar = fst[i];
+    //     // var fstRank = this.bwtRank_(fst[i], i);
+    //     // var sndChar = snd[i];
+    //     // var sndRank = this.sortedBWTRank_(snd[i], i);
+    //     // map[fstChar + String.fromCharCode(fstRank)] =
+    //     //     sndChar + String.fromCharCode(sndRank);
+    //   }
+    //   return map;
+    // },
+    // function bwtRank_(ch, idx) {
+    //   return this.waveletTree_.rank(ch, idx);
+    // },
+    // function sortedBWTRank_(ch, idx) {
+    //   var firstIdx = this.bsFindFirst_(this.sortedBWT_, ch, 0, idx);
+    //   return idx - firstIdx + 1;
+    // },
+    // function bsFind_(arr, value, start, end) {
+    //   var mid;
+    //   while ( start <= end ) {
+    //     mid = Math.floor(start + ((end - start) / 2));
+    //     if ( arr[mid] === value ) return mid;
+    //     if ( arr[mid] < value ) start = mid + 1;
+    //     if ( arr[mid] > value ) end = mid - 1;
+    //   }
+    //   return -1;
+    // },
+    // function bsFindFirst_(arr, value, start, end) {
+    //   var mid;
+    //   while ( start <= end ) {
+    //     mid = Math.floor(start + ((end - start) / 2));
+    //     if ( arr[mid] === value &&
+    //         (mid === 0 || arr[mid - 1] !== value) ) return mid;
+
+    //     if ( arr[mid] > value ) end = mid - 1;
+    //     else                    start = mid + 1;
+    //   }
+    //   return -1;
+    // },
     // TODO(markdittmer): This would be unnecessary if we always stored BWT
     // chars as two-char strings instead of numbers (which happen to have the
     // same number of bits as two-char strings). On the other hand, our binary
     // search would then have to construct numbers from two-char strings.
-    function numToTwoChar_(num) {
-      return String.fromCharCode(num >>> 16) +
-          String.fromCharCode(num & 0x0000FFFF);
-    },
+    // function numToTwoChar_(num) {
+    //   return String.fromCharCode(num >>> 16) +
+    //       String.fromCharCode(num & 0x0000FFFF);
+    // },
     function getFullString_() {
-      var twoChar = this.numToTwoChar_(this.firstcharNum_);
-      var map = this.bwtMap;
+      var bwt = this.bwt;
+      var sortedBWT = this.sortedBWT_;
+      var idx = this.eosIdx_;
       var str = '';
-      while ( map[twoChar] ) {
-        str += twoChar[0];
-        twoChar = map[twoChar];
+      while ( sortedBWT[idx] !== this.eos ) {
+        str += sortedBWT[idx];
+
+        // TODO(markdittmer): Use select queries to locate next bwt index.
       }
+      return str;
     }
     // TODO(markdittmer): Still need a basic reading interface that can
     // construct substrings from a start idx and length.
