e37246697c4274ee394e5b5cc32530f2289f70e7
Mark Dittmer <markdittmer@google.com>
2016-01-25 16:45:25 -0500

    Add comments to DL/FO

--- jasmine/src/fo.js
+++ jasmine/src/fo.js
@@ -1,84 +1,105 @@
 (function(X) {
-  var dl0 = X.set('dl0', Object.create(null, {
+  /**
+   * Axiom 0: A foundational object that, to start, contains the simplest
+   * possible interface for installing axioms.
+   */
+  var a0 = X.set('a0', Object.create(null, {
+    /**
+     * a0.install: Install an axiom directly onto this.
+     */
     install: {
       writable: true, configurable: true, enumerable: true,
-      value: function install(X /* ... DLs ... */) {
-        var dls = X.argsToArray(arguments).slice(1);
-        for (var i = 0; i < dls.length; i++) {
-          dls[i].installOn(X, this);
+      value: function install(X /* ... Axioms ... */) {
+        var as = X.argsToArray(arguments).slice(1);
+        for (var i = 0; i < as.length; i++) {
+          as[i].installOn(X, this);
         }
         return this;
       },
     },
+    /**
+     * a0.installOn: Inverion of control for axiom installation. Allows axioms
+     *               to install themselves onto an arbitrary object.
+     */
     installOn: {
       writable: true, configurable: true, enumerable: true,
       value: function installOn(X, o) {
-        throw new Error('Attempt to install feature without install procedure');
+        throw new Error('Attempt to install description without install '+
+            'procedure');
       },
     },
   }));
 
-  var extend = X.set('extend', Object.create(dl0));
+  /**
+   * extend: Our first real axiom. This elevates install to
+   *         Object.create(this)-then-install.
+   */
+  var extend = X.set('extend', Object.create(a0));
   extend.installOn = function extend_installOn(X, o) {
-    o.extend = function extend(X /* ... DLs ... */) {
+    o.extend = function extend(X /* ... Axioms ... */) {
       var o = Object.create(this);
       return o.install.apply(o, arguments);
     };
   };
-  dl0.install(X, extend);
-  var defDL0 = X.set('defDL0', dl0.extend(X));
-  defDL0.doInstallOn = function defDL0_doInstallOn(X, config, deps) {
-    var base = X.set(config.path, Object.create(config.prototype || X.dl0));
-    base.installOn = function defDL0_installOnWrapper(X, o) {
+  // Install extend into the system.
+  a0.install(X, extend);
+  /**
+   * defA0: Phase 0 of a higher-level interface for defining axioms.
+   *        Implements support for:
+   *        myAxiom = objectWithDefA0Installed.defA({
+   *          path: 'path.to.MyAxiom',
+   *          installRequires: ['foo', 'bar']
+   *          install: function myAxiom_installer(X, o, foo, bar) { ... }
+   *        })
+   *
+   *        Also has chaining of defAs built-in (see handling of defA_).
+   */
+  var defA0 = X.set('defA0', a0.extend(X));
+  defA0.doInstallOn = function defA0_doInstallOn(X, config, deps) {
+    var base = X.set(config.path, Object.create(config.prototype || X.a0));
+    base.installOn = function defA0_installOnWrapper(X, o) {
       return (config.install.code || config.install).apply(
           this, [X, o].concat(deps));
     };
   };
-  defDL0.installOn = function defDL0_installOn(X, o) {
-    var defDL0DL = this;
-    o.defDL_ = [function defDL0(config) {
+  defA0.installOn = function defA0_installOn(X, o) {
+    var defA0A = this;
+    o.defA_ = [function defA0(config) {
       var defX = config.X || X;
-      if (!config.path) throw new Error('Define DL without path');
-      if (!config.install) throw new Error('Define DL without installer');
+      if (!config.path) throw new Error('Define Axiom without path');
+      if (!config.install) throw new Error('Define Axiom without installer');
       return Promise.all(config.installRequires ?
-          config.installRequires.map(function defDL0_mapRequires(path) {
+          config.installRequires.map(function defA0_mapRequires(path) {
             return defX.lookup(path);
-          }) : []).then(defDL0DL.doInstallOn.bind(
-              defDL0DL, defX, config));
+          }) : []).then(defA0A.doInstallOn.bind(
+              defA0A, defX, config));
     }];
-    o.defDL = function defDL(config, opt_X) {
+    o.defA = function defA(config, opt_X) {
       // TODO(markdittmer): Should these run in series instead of parallel?
-      return Promise.all(this.defDL_.map(function defDL_forEach(defDL_) {
-        return defDL_.call(this, config, opt_X);
+      return Promise.all(this.defA_.map(function defA_forEach(defA_) {
+        return defA_.call(this, config, opt_X);
       }.bind(this)));
     };
   };
-  dl0.install(X, defDL0);
+  // Install defA0
+  a0.install(X, defA0);
 
-  var dlPromises = [];
-  // TODO(markdittmer): Do we really have a need for this in bootstrap core?
-  // Only if we need versioned dependency install/uninstall.
-  // dlPromises.push(dl0.defDL({
-  //   path: 'hashCode',
-  //   install: function hashCode_installer(X, o) {
-  //     o.hashCode = function hashCode() {
-  //       var hash = 0;
-  //       var coreStrId = this.installOn.toString();
-  //       if (coreStrId.length == 0) return hash;
+  // Collect axiom initialization promises so we can wait on a bunch of them.
+  var aPromises = [];
 
-  //       for (var i = 0; i < coreStrId.length; i++) {
-  //         var code = coreStrId.charCodeAt(i);
-  //         hash = ((hash << 5) - hash) + code;
-  //         hash &= hash;
-  //       }
-
-  //       return hash;
-  //     };
-  //   },
-  // }));
-
-  dlPromises.push(dl0.defDL({
+  /**
+   * initializable: An axiom that installs initializers that can depend on
+   *                each other. This is a building block for implementing the
+   *                create axiom without running into bad object initialization
+   *                ordering effects.
+   */
+  aPromises.push(a0.defA({
     path: 'initializable',
+    /**
+     * initializable installer: Define initializers_ for bookkeping and
+     *                          installInitializer interface to be used by other
+     *                          axioms.
+     */
     install: function initializable_installer(X, o) {
       o.initializers_ = [];
       o.installInitializer = function installInitializer(
@@ -88,6 +109,8 @@
           deps: deps,
           code: initFn,
         };
+        // Ensure that we have a legitimate place to put initializer in
+        // ordering.
         var i, j, minI = 0, maxI = this.initializers_.length, firstDep, lastDep;
         for (i = this.initializers_.length - 1; i >= 0; i--) {
           if (this.initializers_[i].deps.indexOf(key) >= 0) {
@@ -95,6 +118,7 @@
             firstDep = this.initializers_[i].name;
           }
         }
+        // If possible, just tack new initializer on the end of the list.
         if (maxI === this.initializers_.length) {
           this.initializers_.push(initializer);
           return;
@@ -107,6 +131,8 @@
             }
           }
         }
+        // If deps form a cycle: PANIC.
+        // TODO(markdittmer): This could definitely be improved.
         if (minI > maxI) {
           throw new Error(
               'Irreconcilable init dependencies:' +
@@ -117,39 +143,60 @@
       };
     },
   }));
-  dlPromises.push(dl0.defDL({
-    path: 'defDL1',
-    installRequires: ['defDL0', 'initializable'],
+  /**
+   * defA1: Phase 1 of a higher-level interface for defining axioms.
+   *        Implements support for:
+   *        myAxiom = objectWithDefA0Installed.defA({
+   *          <<Phase 0 definition>>,
+   *          initRequires: ['foo', 'bar']
+   *          init: {
+   *            someCreateArg: function someCreateArg_init(value) { ... },
+   *            ...,
+   *          },
+   *        })
+   *
+   *        Dependencies in initRequires are injected to installInitializer
+   *        to ensure safe initializer ordering.
+   */
+  aPromises.push(a0.defA({
+    path: 'defA1',
+    installRequires: ['defA0', 'initializable'],
     install: {
-      code: function defDL1_installer(X, o, defDL0, initializable) {
-        var defDL1DL = this;
+      code: function defA1_installer(X, o, defA0, initializable) {
+        var defA1A = this;
         // TODO(markdittmer): This should be defined once; not on every install.
-        defDL1DL.doInit = function defDL1_doInit(o, initConfig, deps) {
+        defA1A.doInit = function defA1_doInit(o, initConfig, deps) {
           X.Object_forEach(
               initConfig,
-              function defDL1_doInit_forEach(initializer, key) {
+              function defA1_doInit_forEach(initializer, key) {
                 o.installInitializer(key, deps, initializer);
               });
         };
 
-        if (!o.defDL_) o.install(X, defDL0);
-        o.defDL_.push(function defDL1(config) {
+        if (!o.defA_) o.install(X, defA0);
+        o.defA_.push(function defA1(config) {
           var defX = config.X || X;
           if (config.init) {
             if (!o.installInitializer) o.install(defX, initializable);
             return Promise.resolve(
-                defDL1DL.doInit(o, config.init, config.initRequires || []));
+                defA1A.doInit(o, config.init, config.initRequires || []));
           }
         });
       },
     },
   }));
-  Promise.all(dlPromises).then(function bootstrap_defDL1() {
-    dl0.install(X, X.defDL1);
 
-    dlPromises = [];
+  // Wait for above axioms to finish installing.
+  Promise.all(aPromises).then(function bootstrap_defA1() {
+    aPromises = [];
 
-    dlPromises.push(dl0.defDL({
+    // Install phase-1 axiom-definer.
+    a0.install(X, X.defA1);
+
+    /**
+     * context: The context object axiom. Initializes both X and Y on init.
+     */
+    aPromises.push(a0.defA({
       path: 'context',
       install: function context_installer(X, o) {
         o.X_ = X;
@@ -162,18 +209,20 @@
         },
       },
     }));
-    // TODO(markdittmer): For extends and create:
-    // - Do we want extends_ and prototype_, or just use this?
-    dlPromises.push(dl0.defDL({
+    /**
+     * extends: The model extension axiom. Tracks extended model as well as
+     *          prototype for instances of model.
+     */
+    aPromises.push(a0.defA({
       path: 'extends',
       install: function extends_installer(X, o) {
-        var defaultPrototype = X.dl0 || null;
+        var defaultPrototype = X.a0 || null;
         o.extends_ = defaultPrototype;
         o.prototype_ = defaultPrototype;
       },
       initRequires: ['context'],
       init: {
-        'extends': function extends_init(lookupPath, context) {
+        'extends': function extends_init(lookupPath) {
           this.X.lookup(lookupPath).then(function setPrototype(base) {
             this.extends_ = base;
             this.prototype_ = (base.prototype_ || null);
@@ -181,69 +230,56 @@
         },
       },
     }));
-    dlPromises.push(dl0.defDL({
-    path: 'create',
-    installRequires: ['initializable', 'extends'],
-    install: function create_installer(X, o, initializable, extends_) {
-      o.create = function create(args) {
-        if (!this.prototype_) this.install(X, extends_);
-        var creation = this.prototype_.extend(X);
-        if (!this.initializers_) this.install(X, initializable);
-        X.Object_forEach(args, function forEachCreateArg(arg, name) {
-          if (this.initializers_[name])
-            this.initializers_[name].code.call(this, arg);
-        }.bind(this));
-      };
-    },
+    /**
+     * create: Axiom 0 for modelling. Supports creating an instance from a
+     *         prototype and initializing axioms on the instance.
+     */
+    aPromises.push(a0.defA({
+      path: 'create',
+      installRequires: ['initializable', 'extends'],
+      install: function create_installer(X, o, initializable, extends_) {
+        o.create = function create(args) {
+          if (!this.hasOwnProperty('prototype_')) this.install(X, extends_);
+          if (!this.hasOwnProperty('initializers_'))
+            this.install(X, initializable);
+          var creation = this.prototype_.extend(X);
+          // TODO(markdittmer): We should probably store a parallel map to
+          // speed this up.
+          for (var i = 0; i < this.initializers_.length; i++) {
+            if (args.hasOwnProperty(this.initializers_[i].name)) {
+              this.initializers_[i].code.call(
+                  creation,
+                  args[this.initializers_[i].name]);
+            }
+          }
+          return creation;
+        };
+      },
     }));
 
-    Promise.all(dlPromises).then(function test_Model() {
-      var Model = dl0.extend(X, X['extends']);
+    // Wait for above axioms to finish installing.
+    Promise.all(aPromises).then(function test_Model() {
+      aPromises = [];
 
-      dlPromises = [];
+      // TODO(markdittmer): Install basic features for models.
+
+      var FProto = a0.extend(
+          X,
+          a0.defA({
+            path: 'name',
+            installRequires: ['extends'],
+            install: function name_installer(X, o, extends_) {
+              if (!o.hasOwnProperty('prototype_')) o.install(X, extends_);
+              o.prototype_.name = null;
+            },
+            init: {
+              name: function name_init(name) { this.name = name; },
+            },
+          })
+          // TODO(markdittmer): Install more base FProto axioms.
+          );
 
       debugger;
     });
   });
-
-  // dlPromises.push(dl0.defDL({
-  //   path: 'context',
-  //   install: function context_installer(X, o) {
-  //     o.X_ = X;
-  //     o.X = o.Y = null;
-  //   },
-  //   init: function context_init(opt_X) {
-  //     this.X = opt_X || this.X_;
-  //     this.Y = this.X.sub();
-  //   },
-  // }));
-  // dlPromises.push(dl0.defDL({
-  //   path: 'extends',
-  //   requires: ['context'],
-  //   install: function extends_installer(X, o, initializable, context) {
-  //     var defaultPrototype = X.dl0 || null;
-  //     this.extends_ = defaultPrototype;
-  //     this.prototype_ = defaultPrototype;
-  //   },
-  //   init: function extends_init(lookupPath) {
-  //       this.X.lookup(lookupPath).then(function setPrototype(base) {
-  //         this.extends_ = base;
-  //         this.prototype_ = (base.prototype_ || null);
-  //       }.bind(this));
-  //   },
-  // }));
-  // dlPromises.push(dl0.defDL({
-  //   path: 'create',
-  //   requires: ['initializable', 'extends'],
-  //   install: function create_installer(X, o, initializable, extends_) {
-  //     o.create = function create(args) {
-  //       if (!this.prototype_) this.install(X, extends_);
-  //       var creation = this.prototype_.extend(X);
-  //       if (!this.initializers_) this.install(X, initializable);
-  //       X.Object_forEach(args, function forEachCreateArg(arg, name) {
-  //         if (this.initializers_[name]) this.initializers_[name](arg);
-  //       }.bind(this));
-  //     };
-  //   },
-  // }));
 })(X);
